02b848f4691257e433652289a9327f4f106d7b69
DominikJRau <131611002+DominikJRau@users.noreply.github.com>
2023-04-23 20:38:01 +0200

    Add files via upload

--- tests/test_form1_seleniumkurs_firefox.py
+++ tests/test_form1_seleniumkurs_firefox.py
@@ -1,4 +1,5 @@
 import json
+import requests
 
 from parameterized import parameterized_class
 from selenium import webdriver
@@ -10,7 +11,24 @@ from pages.seleniumkurs_test_app_page import SeleniumKursTestappPage
 from pages.seleniumkurs_testform1_page import SeleniumKursTestform1Page
 
 
-@parameterized_class(json.load(open('https://github.com/DominikJRau/PythonTests/blob/main/tests/test_config.json'))['tests'])
+url = 'https://github.com/DominikJRau/PythonTests/blob/main/tests/test_config.json'
+
+response = requests.get(url)
+if response.status_code == 200:
+    with open('C:/drivers/test_config.json', 'wb') as f:
+        f.write(response.content)
+        print("Die 'test_config.json'-Datei wurde erfolgreich heruntergeladen und gespeichert.")
+else:
+    print("Fehler beim Herunterladen der 'test_config.json'-Datei:", response.status_code)
+
+# Laden der 'test_config.json'-Datei und Verwendung mit @parameterized_class-Decorator
+config_file = open('C:/drivers/test_config.json', 'r')
+config = json.load(config_file)
+
+# Verwendung des 'tests'-Schlüssels aus der 'test_config.json'-Datei im @parameterized_class-Decorator
+@parameterized_class(config['tests'])
+
+#@parameterized_class(json.load(open('https://github.com/DominikJRau/PythonTests/blob/main/tests/test_config.json'))['tests'])
 
 class TestLoginParametrisiertSeleniumkursFirefox(unittest.TestCase):
 
